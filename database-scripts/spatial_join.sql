e216286c09c7addf3545c1c76d4400a15c911012
Romain Fardel <romain.fardel@gmail.com>
2020-09-30 20:53:29 +0200

    Add FIPS join of counties on mortality

---
+++ database-scripts/spatial_join.sql
@@ -0,0 +1,22 @@
+SELECT counties.state as cstate,
+       mortality.state as mstate,
+       counties.countyname,
+       counties.cfips as cfips,
+       mortality.fips as mfips,
+       mortality.year,
+       mortality.month,
+       mortality.weekday,
+       mortality.number,
+FROM mortality
+JOIN counties
+ON (mortality.fips = counties.cfips AND counties.state = mortality.state)
+WHERE counties.countyname like 'Yu%'
+ORDER BY counties.countyname;
+
+/*
+SELECT orders.order_id, suppliers.name
+FROM suppliers
+INNER JOIN orders
+ON suppliers.supplier_id = orders.supplier_id
+ORDER BY order_id;
+*/
