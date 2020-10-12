c549dd04076e93d2dd3c3716526e62a064bf1a1c
Romain Fardel <romain.fardel@gmail.com>
2020-10-12 15:39:04 +0200

    Add SQL summary scripts

--- database-scripts/combined_summary.sql
+++ database-scripts/combined_summary.sql
@@ -2,9 +2,11 @@ SELECT MIN(date) AS earliest_date,
        MAX(date) AS lastest_date,
        COUNT(DISTINCT date) AS nb_days,
        COUNT(DISTINCT date)/365 AS nb_years
-FROM weather;
+FROM combined;
 
-SELECT DATE_PART('year', weather.date) AS year, COUNT(value)
-FROM weather
+SELECT DATE_PART('year', combined.date) AS year,
+       COUNT(DISTINCT countyname) AS "County",
+       COUNT(avg_value) AS "Available data points"
+FROM combined
 GROUP BY year
 ORDER by year;
--- database-scripts/mortality_summary.sql
+++ database-scripts/mortality_summary.sql
@@ -2,9 +2,9 @@ SELECT MIN(date) AS earliest_date,
        MAX(date) AS lastest_date,
        COUNT(DISTINCT date) AS nb_days,
        COUNT(DISTINCT date)/365 AS nb_years
-FROM weather;
+FROM mortality;
 
-SELECT DATE_PART('year', weather.date) AS year, COUNT(value)
-FROM weather
+SELECT DATE_PART('year', mortality.date) AS year, COUNT(number)
+FROM mortality
 GROUP BY year
 ORDER by year;
